0b02588e12be531f9ed744d254d72c8a61748307
Travis Johnson <trav.johnson919@gmail.com>
2016-07-01 17:20:36 -0400

    added template

--- assets/scripts/controllers.js
+++ assets/scripts/controllers.js
@@ -1,8 +1,36 @@
-mOverP.controller('modal', function($scope){
+// mOverP.controller('modal', function($scope){
+//   $scope.showMenu=false;
+//   $scope.modalFunc= function(){
+//     $scope.showMenu = !$scope.showMenu
+//   }
+// });
+
+mOverP.controller('store', function($scope, $http ){
   $scope.showMenu=false;
   $scope.modalFunc= function(){
-    $scope.showMenu = !$scope.showMenu
-  }
+    $scope.showMenu = !$scope.showMenu;
+  };
+  var loadTrack= ["metal", "solid", "grind", "follow", "iron", "yard", "cats", "sailor%20moon", "dogs", "smoke", "mirror", "magic", "cool", "virtual", "reality", "phone", "lemon", "next", "banana",
+  "preacher", "shark", "romeo"  ];
+  var searchRand= Math.floor((Math.random() * loadTrack.length));
+  console.log(searchRand);
+  $http({
+    method: 'GET',
+    url: 'https://api.spotify.com/v1/search?q=' + loadTrack[searchRand] + '&type=track&limit=9',
+  }).then(function(album){
+    // $scope.album= album.data;
+    // for (var i = 0; i <=8; i++)
+    $scope.albums = [{
+      title: album.data.tracks.items[0].name,
+      artist: album.data.tracks.items[0].artists[0].name
+
+    }]
+
+    //$scope.artist = album.data.
+    console.log(album);
+    console.log(album.data.tracks.items[0].artists[0].name);
+
+  });
 });
 
 mOverP.controller('admin',function($scope, $http, $route){
