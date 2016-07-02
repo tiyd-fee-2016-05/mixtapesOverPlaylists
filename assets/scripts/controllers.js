2aaf79a3fb7a74c16b518318f1ecf577d48225fc
Aaron <newcomer.aaron@icloud.com>
2016-07-02 12:27:06 -0400

    added cool repeat function

--- assets/scripts/controllers.js
+++ assets/scripts/controllers.js
@@ -10,6 +10,9 @@ mOverP.controller('store', function($scope, $http ){
   $scope.modalFunc= function(){
     $scope.showMenu = !$scope.showMenu;
   };
+  // $scope.tunes= function(){
+  //   console.log("hello");
+  // }
   var loadTrack= ["metal", "solid", "grind", "follow", "iron", "yard", "cats", "sailor%20moon", "dogs", "smoke", "mirror", "magic", "cool", "virtual", "reality", "phone", "lemon", "next", "banana",
   "preacher", "shark", "romeo"  ];
   var searchRand= Math.floor((Math.random() * loadTrack.length));
@@ -17,19 +20,63 @@ mOverP.controller('store', function($scope, $http ){
   $http({
     method: 'GET',
     url: 'https://api.spotify.com/v1/search?q=' + loadTrack[searchRand] + '&type=track&limit=9',
-  }).then(function(album){
+  }).then($scope.tunesFunc = function(album){
     // $scope.album= album.data;
-    // for (var i = 0; i <=8; i++)
+    //for (var i = 0; i <=8; i++){
     $scope.albums = [{
       title: album.data.tracks.items[0].name,
-      artist: album.data.tracks.items[0].artists[0].name
+      artist: album.data.tracks.items[0].artists[0].name,
+      img: album.data.tracks.items[0].album.images[1].url
+    },
+    {
+      title: album.data.tracks.items[1].name,
+      artist: album.data.tracks.items[1].artists[0].name,
+      img: album.data.tracks.items[1].album.images[1].url
+    },
+    {
+      title: album.data.tracks.items[2].name,
+      artist: album.data.tracks.items[2].artists[0].name,
+      img: album.data.tracks.items[2].album.images[1].url
+
+    },
+    {
+      title: album.data.tracks.items[3].name,
+      artist: album.data.tracks.items[3].artists[0].name,
+      img: album.data.tracks.items[3].album.images[1].url
+    },
+    {
+      title: album.data.tracks.items[4].name,
+      artist: album.data.tracks.items[4].artists[0].name,
+      img: album.data.tracks.items[4].album.images[1].url
+    },
+    {
+      title: album.data.tracks.items[5].name,
+      artist: album.data.tracks.items[5].artists[0].name,
+      img: album.data.tracks.items[5].album.images[1].url
+
+    },
+    {
+      title: album.data.tracks.items[6].name,
+      artist: album.data.tracks.items[6].artists[0].name,
+      img: album.data.tracks.items[6].album.images[1].url
+    },
+    {
+      title: album.data.tracks.items[7].name,
+      artist: album.data.tracks.items[7].artists[0].name,
+      img: album.data.tracks.items[7].album.images[1].url
+
+    },
+    {
+      title: album.data.tracks.items[8].name,
+      artist: album.data.tracks.items[8].artists[0].name,
+      img: album.data.tracks.items[8].album.images[1].url
 
     }]
 
-    //$scope.artist = album.data.
-    console.log(album);
-    console.log(album.data.tracks.items[0].artists[0].name);
-
+    console.log($scope.albums);
+    //console.log(i);
+    console.log(album.data);
+  //};
   });
 });
 
