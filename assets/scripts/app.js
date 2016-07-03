15cfdde7bae751b57ea4c5af91045f1eafd8867f
Aaron <newcomer.aaron@icloud.com>
2016-07-03 16:37:23 -0400

    added delete request

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,21 +3,29 @@ var mOverP = angular.module("mOverP",['ngRoute']);
 mOverP.config(function($routeProvider) {
   $routeProvider
   .when('/landing',{
-    templateUrl:'index/landing.html'
+    templateUrl:'index/landing.html',
+    css: '../styles/landing.css'
   })
   .when('/cart',{
-    templateUrl:'index/cart.html'
+    templateUrl:'index/cart.html',
+    controller: 'cart',
+    css: '../styles/cart.css'
+
   })
   .when('/admin',{
     templateUrl: 'index/admin.html',
-    controller: 'admin'
+    controller: 'admin',
+    css:'../styles/admin.css'
   })
   .when('/store',{
     templateUrl:'index/store.html',
-    controller: 'store'
+    controller: 'store',
+    css: '../styles/store.css'
   })
   .when('/receipt',{
-    templateUrl: 'index/receipt.html'
+    templateUrl: 'index/receipt.html',
+    controller: 'cart',
+    css: '../styles/receipt.css'
   })
   .otherwise({
     redirectTo: '/landing'
